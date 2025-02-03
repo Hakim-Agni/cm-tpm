cc4db5988417048ec763c1a9d623438137f7dc68
Hakim-Agni <hakim.a@kpnmail.nl>
2025-02-03 20:55:49 +0100

    Added initial helper functions and tests

--- src/cm_tpm/_cm.py
+++ src/cm_tpm/_cm.py
@@ -1,41 +1,57 @@
+import numpy as np
+import pandas as pd
 from cm_tpm.cpp._add import add
 from cm_tpm.cpp._multiply import multiply
 from cm_tpm.cpp._subtract import subtract
 from cm_tpm.cpp._divide import divide
 
-class CMImputer():
+class CMImputer:
     """
-    
-    """
-    def __init__(self):
-        super().__init__()
+    Imputation for completing missing values using Continuous Mixtures of 
+    Tractable Probabilistic Models.
 
-    def test(self):
-        """
-        Test the module
-        """
-        return 1
-    
-    def add(self, x, y):
-        """
-        Add two numbers
-        """
-        return add(x, y)
-    
-    def multiply(self, x, y):
-        """
-        Multiply two numbers
-        """
-        return multiply(x, y)
-    
-    def subtract(self, x, y):
-        """
-        Subtract two numbers
-        """
-        return subtract(x, y)
-    
-    def divide(self, x, y):
-        """
-        Divide two numbers
-        """
-        return divide(x, y)
+    Parameters
+    ----------
+    random_state: int, RandomState instance or None, optional (default=None)
+        Random seed for reproducibility
+
+    Attributes
+    ----------
+    ...
+
+    References
+    ----------
+    ...
+
+    Examples
+    --------
+    ...
+    """
+    def __init__(self, random_state: int = None):
+        super().__init__()
+        self.random_state = random_state
+
+    def _load_file(self, filepath: str) -> pd.DataFrame:
+        """Loads a dataset from a file into a pandas DataFrame."""
+        if filepath.endswith('.csv'):
+            # TODO: Add support for other separators and decimal markers
+            return pd.read_csv(filepath, sep=';', decimal=',')
+        elif filepath.endswith('.xlsx'):
+            return pd.read_excel(filepath, engine="openpyxl")
+        elif filepath.endswith('.parquet'):
+            return pd.read_parquet(filepath)
+        elif filepath.endswith('.feather'):
+            return pd.read_feather(filepath)
+        else:
+            raise ValueError("Unsupported file format. Please provide a CSV, Excel, Parquet, or Feather file.")
+        
+    def _to_numpy(self, X):
+        "Converts input data to NumPy array for internal processing."
+        if isinstance(X, pd.DataFrame):
+            return X.to_numpy(), "DataFrame", X.columns
+        elif isinstance(X, list):
+            return np.array(X), "list", None
+        elif isinstance(X, np.ndarray):
+            return X, "ndarray", None
+        else:
+            raise ValueError("Unsupported data type. Please provide a NumPy array, pandas DataFrame or list.")
---
+++ tests/test_cm.py
@@ -0,0 +1,135 @@
+import pytest
+import numpy as np
+import pandas as pd
+from cm_tpm import CMImputer
+
+class TestClass:
+    def test_instance(self):
+        """
+        Test the instantiation of the CMImputer class.
+        """
+        imputer = CMImputer()
+        assert imputer is not None
+
+    def test_random_state(self):
+        """
+        Test the random_state parameter.
+        """
+        imputer = CMImputer(random_state=42)
+        assert imputer.random_state == 42
+
+class TestLoadFiletypes:
+    @pytest.fixture(autouse=True)
+    def setup_method(self):
+        """
+        Setup method for the test class.
+        """
+        self.imputer = CMImputer()
+
+    def test_load_csv_file(self):
+        """
+        Test loading a CSV file.
+        """
+        df = self.imputer._load_file("tests/data/test_data.csv")
+        assert df.shape == (10, 3)
+        assert df.columns.tolist() == ["A", "B", "C"]
+        assert df.dtypes.tolist() == [float, float, float]
+
+    def test_load_xlsx_file(self):
+        """
+        Test loading a XLSX file.
+        """
+        df = self.imputer._load_file("tests/data/test_data.xlsx")
+        assert df.shape == (10, 3)
+        assert df.columns.tolist() == ["A", "B", "C"]
+        assert df.dtypes.tolist() == [float, float, float]
+
+    def test_load_parquet_file(self):
+        """
+        Test loading a Parquet file.
+        """
+        df = self.imputer._load_file("tests/data/test_data.parquet")
+        assert df.shape == (10, 3)
+        assert df.columns.tolist() == ["A", "B", "C"]
+        assert df.dtypes.tolist() == [float, float, float]
+
+    def test_load_feather_file(self):
+        """
+        Test loading a Feather file.
+        """
+        df = self.imputer._load_file("tests/data/test_data.feather")
+        assert df.shape == (10, 3)
+        assert df.columns.tolist() == ["A", "B", "C"]
+        assert df.dtypes.tolist() == [float, float, float]
+
+    def test_unsupported_file_format(self):
+        """
+        Test loading an unsupported filetype.
+        """
+        try:
+            self.imputer._load_file("tests/data/test_data.txt")
+            assert False
+        except ValueError as e:
+            assert str(e) == "Unsupported file format. Please provide a CSV, Excel, Parquet, or Feather file."
+
+    def test_file_not_exists(self):
+        """
+        Test loading a file that does not exist.
+        """
+        try:
+            self.imputer._load_file("tests/data/non_existent_file.csv")
+            assert False
+        except FileNotFoundError as e:
+            assert str(e) == "[Errno 2] No such file or directory: 'tests/data/non_existent_file.csv'"
+
+class TestToNumpy():
+    @pytest.fixture(autouse=True)
+    def setup_method(self):
+        """
+        Setup method for the test class.
+        """
+        self.imputer = CMImputer()
+
+    def test_dataframe_to_numpy(self):
+        """
+        Test converting a pandas DataFrame to a NumPy array.
+        """
+        self.imputer = CMImputer()
+        df = pd.DataFrame({"A": [1, 2, 3], "B": [4, 5, 6]})
+        X_np, original_format, columns = self.imputer._to_numpy(df)
+        assert X_np.shape == (3, 2)
+        assert isinstance(X_np, np.ndarray)
+        assert original_format == "DataFrame"
+        assert columns[0] == "A"
+        assert columns[1] == "B"
+
+    def test_list_to_numpy(self):
+        """
+        Test converting a list to a NumPy array.
+        """
+        X_np, original_format, columns = self.imputer._to_numpy([1, 2, 3])
+        assert X_np.shape == (3,)
+        assert isinstance(X_np, np.ndarray)
+        assert original_format == "list"
+        assert columns is None
+
+    def test_ndarray_to_numpy(self):
+        """
+        Test converting a NumPy array to a NumPy array.
+        """
+        X_np, original_format, columns = self.imputer._to_numpy(np.array([1, 2, 3]))
+        assert X_np.shape == (3,)
+        assert isinstance(X_np, np.ndarray)
+        assert original_format == "ndarray"
+        assert columns is None
+
+    def test_other_to_numpy(self):
+        """
+        Test converting other data types to a NumPy array.
+        """
+        try:
+            X_np, original_format, columns = self.imputer._to_numpy("test")
+            assert False
+        except ValueError as e:
+            assert str(e) == "Unsupported data type. Please provide a NumPy array, pandas DataFrame or list."
+     
