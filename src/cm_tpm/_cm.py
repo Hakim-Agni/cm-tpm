b1684b9686c8839fb4b4b139bbfaee0e1b66edd7
HakimA2001 <54859731+Hakim-Agni@users.noreply.github.com>
2025-02-03 23:56:25 +0100

    Added formatting logic

--- src/cm_tpm/_cm.py
+++ src/cm_tpm/_cm.py
@@ -46,7 +46,7 @@ class CMImputer:
             raise ValueError("Unsupported file format. Please provide a CSV, Excel, Parquet, or Feather file.")
         
     def _to_numpy(self, X):
-        "Converts input data to NumPy array for internal processing."
+        """Converts input data to NumPy array for internal processing."""
         if isinstance(X, pd.DataFrame):
             return X.to_numpy(), "DataFrame", X.columns
         elif isinstance(X, list):
@@ -55,3 +55,93 @@ class CMImputer:
             return X, "ndarray", None
         else:
             raise ValueError("Unsupported data type. Please provide a NumPy array, pandas DataFrame or list.")
+        
+    def _restore_format(self, X_imputed, original_format="ndarray", columns=None):
+        """Restore the format of the imputed data based on the original input format."""
+        if original_format == "DataFrame":
+            return pd.DataFrame(X_imputed, columns=columns)
+        elif original_format == "list":
+            return X_imputed.tolist()
+        return X_imputed
+
+    def fit(self, X: str | np.ndarray | pd.DataFrame | list) -> "CMImputer":
+        """
+        Fit the imputation model to the input dataset
+
+        Parameters:
+            X (array-like or str): Input data with missing values.
+                - Allowed: np.ndarray, pd.DataFrame, list of lists, or a file path (CSV, XLSX, Parquest, Feather)
+
+        Returns:
+            self (CMImputer): Fitted instance.        
+        """
+        # If the input data is a string (filepath), load the data from the file
+        if isinstance(X, str):
+            X = self._load_file(X)
+        # Transform the data to a NumPy array
+        X, _, _ = self._to_numpy(X)
+        # Fit the model using X
+        # TODO
+
+        return self
+    
+    def transform(self, X: str | np.ndarray | pd.DataFrame | list, save_path: str = None):
+        """
+        Impute missing values in the dataset.
+
+        Parameters:
+            X (array-like or str): Data with missing values.
+                - Allowed: np.ndarray, pd.DataFrame, list of lists, or a file path (CSV, XLSX, Parquest, Feather)
+            save_path (str, optional): If provided, saves output to a file. Otherwise, if X is a filepath, save output to 'X + _imputed'.
+
+        Returns:
+            X_imputed (array-like, same type as X): Dataset with missing values replaced. If X is a filepath, X imputed will be a NumPy array.
+        """
+        # If the input data is a string (filepath), load the data from the file
+        if isinstance(X, str):
+            file_in = X
+            X, _, _ = self._to_numpy(self._load_file(X))
+        # Transform the data to a NumPy array
+        X_np, original_format, columns = self._to_numpy(X)
+        # Perfom imputation
+        X_imputed = self._impute(X_np)
+        # Transform the imputed data to the original format
+        result = self._restore_format(X_imputed, original_format, columns)
+        
+        # If save_path is set, save the imputed data to a file
+        if save_path or file_in:
+            if not save_path:
+                save_path = file_in[:file_in.rfind(".")] + "_imputed" + file_in[file_in.rfind("."):]
+            if save_path.endswith(".csv"):
+                pd.DataFrame(result, columns=columns).to_csv(save_path, index=False)
+            elif save_path.endswith(".xlsx"):
+                pd.DataFrame(result, columns=columns).to_excel(save_path, index=False, engine="openpyxl")
+            elif save_path.endswith(".parquet"):
+                pd.DataFrame(result, columns=columns).to_parquet(save_path)
+            elif save_path.endswith(".feather"):
+                pd.DataFrame(result, columns=columns).to_feather(save_path)
+            else:
+                raise ValueError("Unsupported file format for saving.")
+
+        # Return the imputed data
+        return result
+    
+    def fit_transform(self, X: str | np.ndarray | pd.DataFrame | list, save_path:str = None):
+        """
+        Fit the model and then transform the data.
+
+        Parameters:
+            X (array-like or str): Data with missing values.
+                - Allowed: np.ndarray, pd.DataFrame, list of lists, or a file path (CSV, XLSX, Parquest, Feather)
+            save_path (str, optional): If provided, saves output to a file. Otherwise, if X is a filepath, save output to 'X + _imputed'.
+
+        Returns:
+            X_imputed (array-like, same type as X): Dataset with missing values replaced. If X is a filepath, X imputed will be a NumPy array.
+        """
+        return self.fit(X).transform(X, save_path)
+    
+    def _impute(self, X: np.ndarray) -> np.ndarray:
+        """Placeholder for the actual imputation logic"""
+        # TODO Add imputation
+        return X
+    
--- tests/test_cm.py
+++ tests/test_cm.py
@@ -5,67 +5,51 @@ from cm_tpm import CMImputer
 
 class TestClass:
     def test_instance(self):
-        """
-        Test the instantiation of the CMImputer class.
-        """
+        """Test the instantiation of the CMImputer class."""
         imputer = CMImputer()
         assert imputer is not None
 
     def test_random_state(self):
-        """
-        Test the random_state parameter.
-        """
+        """Test the random_state parameter."""
         imputer = CMImputer(random_state=42)
         assert imputer.random_state == 42
 
 class TestLoadFiletypes:
     @pytest.fixture(autouse=True)
     def setup_method(self):
-        """
-        Setup method for the test class.
-        """
+        """Setup method for the test class."""
         self.imputer = CMImputer()
 
     def test_load_csv_file(self):
-        """
-        Test loading a CSV file.
-        """
+        """Test loading a CSV file."""
         df = self.imputer._load_file("tests/data/test_data.csv")
         assert df.shape == (10, 3)
         assert df.columns.tolist() == ["A", "B", "C"]
         assert df.dtypes.tolist() == [float, float, float]
 
     def test_load_xlsx_file(self):
-        """
-        Test loading a XLSX file.
-        """
+        """Test loading a XLSX file."""
         df = self.imputer._load_file("tests/data/test_data.xlsx")
         assert df.shape == (10, 3)
         assert df.columns.tolist() == ["A", "B", "C"]
         assert df.dtypes.tolist() == [float, float, float]
 
     def test_load_parquet_file(self):
-        """
-        Test loading a Parquet file.
-        """
+        """Test loading a Parquet file."""
         df = self.imputer._load_file("tests/data/test_data.parquet")
         assert df.shape == (10, 3)
         assert df.columns.tolist() == ["A", "B", "C"]
         assert df.dtypes.tolist() == [float, float, float]
 
     def test_load_feather_file(self):
-        """
-        Test loading a Feather file.
-        """
+        """Test loading a Feather file."""
         df = self.imputer._load_file("tests/data/test_data.feather")
         assert df.shape == (10, 3)
         assert df.columns.tolist() == ["A", "B", "C"]
         assert df.dtypes.tolist() == [float, float, float]
 
     def test_unsupported_file_format(self):
-        """
-        Test loading an unsupported filetype.
-        """
+        """Test loading an unsupported filetype."""
         try:
             self.imputer._load_file("tests/data/test_data.txt")
             assert False
@@ -73,9 +57,7 @@ class TestLoadFiletypes:
             assert str(e) == "Unsupported file format. Please provide a CSV, Excel, Parquet, or Feather file."
 
     def test_file_not_exists(self):
-        """
-        Test loading a file that does not exist.
-        """
+        """Test loading a file that does not exist."""
         try:
             self.imputer._load_file("tests/data/non_existent_file.csv")
             assert False
@@ -85,51 +67,76 @@ class TestLoadFiletypes:
 class TestToNumpy():
     @pytest.fixture(autouse=True)
     def setup_method(self):
-        """
-        Setup method for the test class.
-        """
+        """Setup method for the test class."""
         self.imputer = CMImputer()
 
     def test_dataframe_to_numpy(self):
-        """
-        Test converting a pandas DataFrame to a NumPy array.
-        """
+        """Test converting a pandas DataFrame to a NumPy array."""
         self.imputer = CMImputer()
         df = pd.DataFrame({"A": [1, 2, 3], "B": [4, 5, 6]})
         X_np, original_format, columns = self.imputer._to_numpy(df)
-        assert X_np.shape == (3, 2)
         assert isinstance(X_np, np.ndarray)
+        assert X_np.shape == (3, 2)
         assert original_format == "DataFrame"
         assert columns[0] == "A"
         assert columns[1] == "B"
 
     def test_list_to_numpy(self):
-        """
-        Test converting a list to a NumPy array.
-        """
+        """Test converting a list to a NumPy array."""
         X_np, original_format, columns = self.imputer._to_numpy([1, 2, 3])
-        assert X_np.shape == (3,)
         assert isinstance(X_np, np.ndarray)
+        assert X_np.shape == (3,)
         assert original_format == "list"
         assert columns is None
 
-    def test_ndarray_to_numpy(self):
-        """
-        Test converting a NumPy array to a NumPy array.
-        """
+    def test_numpy_to_numpy(self):
+        """Test converting a NumPy array to a NumPy array."""
         X_np, original_format, columns = self.imputer._to_numpy(np.array([1, 2, 3]))
-        assert X_np.shape == (3,)
         assert isinstance(X_np, np.ndarray)
+        assert X_np.shape == (3,)
         assert original_format == "ndarray"
         assert columns is None
 
     def test_unsupported_to_numpy(self):
-        """
-        Test converting unsupported data types to a NumPy array.
-        """
+        """Test converting unsupported data types to a NumPy array."""
         try:
-            X_np, original_format, columns = self.imputer._to_numpy("test")
+            _, _, _ = self.imputer._to_numpy("test")
             assert False
         except ValueError as e:
             assert str(e) == "Unsupported data type. Please provide a NumPy array, pandas DataFrame or list."
      
+class TestRestoreFormat():
+    @pytest.fixture(autouse=True)
+    def setup_method(self):
+        """Setup method for the test class."""
+        self.imputer = CMImputer()
+        self.X_imputed = np.array([[1, 2, 3], [4, 5, 6]])
+
+    def test_restore_dataframe(self):
+        """Test restoring data to DataFrame format"""
+        columns = ["A", "B", "C"]
+        restored = self.imputer._restore_format(self.X_imputed, original_format="DataFrame", columns=columns)
+        assert isinstance(restored, pd.DataFrame)
+        assert restored.shape == (2, 3)
+        assert restored.columns[0] == columns[0]
+        assert restored.columns[1] == columns[1]
+        assert restored.columns[2] == columns[2]
+
+    def test_restore_list(self):
+        """Test restoring data to list format"""
+        restored = self.imputer._restore_format(self.X_imputed, original_format="list")
+        assert isinstance(restored, list)
+        assert len(restored) == 2
+        assert len(restored[0]) == 3
+
+    def test_restore_numpy(self):
+        """Test restoring data to NumPy array"""
+        restored = self.imputer._restore_format(self.X_imputed, original_format="ndarray")
+        assert isinstance(restored, np.ndarray)
+        assert restored.shape == (2, 3)
+
+    def test_restore_default(self):
+        """Test that data is restored to NumPy array by default"""
+        restored = self.imputer._restore_format(self.X_imputed)
+        assert isinstance(restored, np.ndarray)
+        assert restored.shape == (2, 3)
