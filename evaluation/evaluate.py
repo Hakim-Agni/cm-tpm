fb26bee5fe42432f80379e64b6da31a79ae1ea42
Hakim-Agni <hakim.a@kpnmail.nl>
2025-04-10 17:02:03 +0200

    Added more datasets

--- evaluation/evaluate.py
+++ evaluation/evaluate.py
@@ -6,23 +6,34 @@ from torch import nn
 from sklearn.metrics import mean_absolute_error, mean_squared_error
 from sklearn.datasets import load_diabetes, load_breast_cancer, load_digits, load_iris, load_linnerud, load_wine
 from sklearn.impute import KNNImputer, SimpleImputer
+from ucimlrepo import fetch_ucirepo
 from cm_tpm import CMImputer
 
 
 # Dataset Settings
-diabetes = True                 # Medium sized, numerical and integer
-breast_cancer = True            # Large sized, numerical and binary
-digits = True                   # Very large sized, integer
-iris = True                     # Small sized, numerical and binary
-linnerud = True                 # Small sized, integer
-wine = True                     # Medium sized, numerical and binary
+# Complete datasets
+diabetes = False                 # Medium sized; numerical and integer
+breast_cancer = False            # Large sized; numerical and binary
+digits = False                   # Very large sized; integer
+iris = False                     # Small sized; numerical and binary
+linnerud = False                 # Small sized; integer
+mushroom = True                  # Very large sized; categorical and binary
+wine = False                     # Medium sized; numerical and binary
+
+# Datasets with missing values
+credit = False                     # Large sized; numerical, integer and categorical
+titanic = False                   # Large sized; numerical, categorical and binary
+
 datasets = {
     "diabetes": diabetes,
     "breast_cancer": breast_cancer,
     "digits": digits,
     "iris": iris,
     "linnerud": linnerud,
+    "mushroom": mushroom,
     "wine": wine,
+    "credit": credit,
+    "titanic": titanic,
     }
 
 # Imputer Settings
@@ -90,6 +101,9 @@ for dataset_name, use_dataset in datasets.items():
     if not use_dataset:
         continue
 
+    categorical = False
+    missing = False
+
     if dataset_name == "diabetes":
         # Load dataset as a pandas DataFrame
         data = load_diabetes(as_frame=True).frame
@@ -111,15 +125,36 @@ for dataset_name, use_dataset in datasets.items():
         data = load_linnerud(as_frame=True).frame
         os.makedirs("evaluation/data/linnerud", exist_ok=True)
         path = "evaluation/data/linnerud/linnerud_"
+    elif dataset_name == "mushroom":
+        categorical = True
+        mushroom = fetch_ucirepo(id=73)
+        data = pd.DataFrame(mushroom.data.features, columns=mushroom.feature_names)
+        os.makedirs("evaluation/data/mushroom", exist_ok=True)
+        path = "evaluation/data/mushroom/mushroom_"
     elif dataset_name == "wine":
         data = load_wine(as_frame=True).frame
         os.makedirs("evaluation/data/wine", exist_ok=True)
         path = "evaluation/data/wine/wine_"
+    elif dataset_name == "credit":
+        categorical = True
+        missing = True
+        credit = fetch_ucirepo(id=27)
+        data = pd.DataFrame(credit.data.features, columns=credit.feature_names)
+        data_missing = data.copy()
+        os.makedirs("evaluation/data/credit", exist_ok=True)
+        path = "evaluation/data/credit/credit_"
+    elif dataset_name == "titanic":
+        categorical = True
+        missing = True
+        data = pd.read_csv("evaluation/data/titanic/titanic_complete.csv")
+        data_missing = data.copy()
+        path = "evaluation/data/titanic/titanic_"
 
     data.to_csv(path + "complete.csv", index=False)
 
     # Introduce 10% missing values
-    data_missing, mask = introduce_missingness(data, missing_rate=0.1)
+    if not missing:
+        data_missing, mask = introduce_missingness(data, missing_rate=0.1)
 
     # Display missing value summary
     # print(data_missing.isnull().sum())
@@ -131,6 +166,8 @@ for dataset_name, use_dataset in datasets.items():
     # Impute missing values using chosen imputers
     for imputer_name, use_imputer in imputers.items():
         if not use_imputer:
+            continue
+        if imputer_name != "cm_imputer" and categorical:
             continue
 
         if imputer_name == "cm_imputer":
@@ -157,20 +194,23 @@ for dataset_name, use_dataset in datasets.items():
             data_imputed.to_csv(path + "imputed_cm.csv", index=False)
             print(f"Imputed dataset saved as '{dataset_name}_imputed_cm.csv'")
 
-        # Select only the originally missing values for comparison
-        imputed_values = data_imputed.values[mask]
-        true_values = data.values[mask]
-
-        # Compute Error Metrics
-        mae = mean_absolute_error(true_values, imputed_values)
-        rmse = np.sqrt(mean_squared_error(true_values, imputed_values))
-        correlation = np.corrcoef(true_values.flatten(), imputed_values.flatten())[0, 1]
-
         # Print evaluation results
         print("___________________________________________________")
         print(f"{name} evaluation on {dataset_name} dataset:")
         print(f"Time taken for imputation: {end_time - start_time:.2f} seconds")
-        print(f"Mean Absolute Error (MAE): {mae:.4f}")
-        print(f"Root Mean Squared Error (RMSE): {rmse:.4f}")
-        print(f"Correlation between true and imputed values: {correlation:.4f}")
+        if not missing:
+            # Select only the originally missing values for comparison
+            imputed_values = data_imputed.values[mask]
+            true_values = data.values[mask]
+            if not categorical:     # For non-categorical datasets
+                # Compute Error Metrics
+                mae = mean_absolute_error(true_values, imputed_values)
+                rmse = np.sqrt(mean_squared_error(true_values, imputed_values))
+                correlation = np.corrcoef(true_values.flatten(), imputed_values.flatten())[0, 1]
+                print(f"Mean Absolute Error (MAE): {mae:.4f}")
+                print(f"Root Mean Squared Error (RMSE): {rmse:.4f}")
+                print(f"Correlation between true and imputed values: {correlation:.4f}")
+            else:       # For categorical datasets
+                accuracy = (imputed_values == true_values).mean()
+                print(f"Accuracy of imputed values: {accuracy:.4f}")
     print("___________________________________________________")
