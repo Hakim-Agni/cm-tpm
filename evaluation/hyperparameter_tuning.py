db322997a743b0db55f21bbc274bd90245405803
Hakim-Agni <hakim.a@kpnmail.nl>
2025-04-23 21:46:19 +0200

    Seperate file for hyperparameter tuning

--- evaluation/hyperparameter_tuning.py
+++ evaluation/hyperparameter_tuning.py
@@ -12,12 +12,12 @@ from cm_tpm import CMImputer
 
 # Dataset Settings
 # Complete datasets
-diabetes = True                 # Medium sized; numerical and integer
+diabetes = False                 # Medium sized; numerical and integer
 breast_cancer = False            # Large sized; numerical and binary
 digits = False                   # Very large sized; integer; image
 iris = False                     # Small sized; numerical and binary
 linnerud = False                 # Small sized; integer
-mushroom = False                  # Very large sized; categorical and binary
+mushroom = True                  # Very large sized; categorical and binary
 wine = False                     # Medium sized; numerical and binary
 
 # Datasets with missing values
@@ -218,38 +218,87 @@ def run_evaluation(cm_imputer=CMImputer(), print_results=True):
 if __name__ == "__main__":
     # CMImputer Settings
     hidden_layers = 5
-    neurons_per_layer = 512
-    activation = "LeakyReLU"
+    neurons_per_layer = 1024
+    activation = "Identity"
     batch_norm = True
-    dropout_rate = 0.1
+    dropout_rate = 0.3
 
-    cm_imputer = CMImputer(
-        missing_values=np.nan,
-        n_components_train=256,
-        n_components_impute=1024,
-        latent_dim=4,
-        top_k=None,
-        lo=False,
-        pc_type="factorized",
-        ordinal_features=None,
-        max_depth=5,
-        custom_net=None,
-        hidden_layers=hidden_layers,
-        neurons_per_layer=neurons_per_layer,
-        activation=activation,
-        batch_norm=batch_norm,
-        dropout_rate=dropout_rate,
-        max_iter=100,
-        batch_size=None,
-        tol=0.0001,
-        lr=0.001,
-        weight_decay=0.01,
-        use_gpu=True,
-        random_state=0,
-        verbose=1,
-        copy=True,
-        keep_empty_features=True,
-    )
+    random_states = [0, 42, 100, 35, 8]
 
-    run_evaluation(cm_imputer=cm_imputer, print_results=True)
+    maes = {}
+    accs = {}
+    tt = {}
+    ti = {}
+
+    for i in range(len(random_states)):
+        cm_imputer = CMImputer(
+            missing_values=np.nan,
+            n_components_train=256,
+            n_components_impute=1024,
+            latent_dim=4,
+            top_k=None,
+            lo=False,
+            pc_type="factorized",
+            ordinal_features=None,
+            max_depth=5,
+            custom_net=None,
+            hidden_layers=hidden_layers,
+            neurons_per_layer=neurons_per_layer,
+            activation=activation,
+            batch_norm=batch_norm,
+            dropout_rate=dropout_rate,
+            max_iter=100,
+            batch_size=None,
+            tol=0.0001,
+            lr=0.001,
+            weight_decay=0.01,
+            use_gpu=True,
+            random_state=random_states[i],
+            verbose=0,
+            copy=True,
+            keep_empty_features=True,
+        )
+
+        score = run_evaluation(cm_imputer=cm_imputer, print_results=False)
         
+        for dataset_name, imputer_name in score.keys():
+            if imputer_name != "cm_imputer":
+                continue
+
+            if dataset_name not in maes:
+                maes[dataset_name] = []
+                accs[dataset_name] = []
+                tt[dataset_name] = []
+                ti[dataset_name] = []
+
+            mae = score[dataset_name, imputer_name].get("MAE", 0)
+            accuracy = score[dataset_name, imputer_name].get("Accuracy", 0)
+            time_train = score[dataset_name, imputer_name].get("Training Time", 0)
+            time_impute = score[dataset_name, imputer_name].get("Imputation Time", 0)
+
+            maes[dataset_name].append(mae)
+            accs[dataset_name].append(accuracy)
+            tt[dataset_name].append(time_train)
+            ti[dataset_name].append(time_impute)
+    
+    # print(f"Mean Absolute Error (MAE):")
+    # for dataset_name in maes.keys():
+    #     print(f"\t{dataset_name}: {np.mean(maes[dataset_name]):.4f}")
+    # print(f"Accuracy:")
+    # for dataset_name in accs.keys():
+    #     print(f"\t{dataset_name}: {np.mean(accs[dataset_name]):.4f}")
+    # print(f"Training Time:")
+    # for dataset_name in tt.keys():
+    #     print(f"\t{dataset_name}: {np.mean(tt[dataset_name]):.4f} seconds")
+    # print(f"Imputation Time:")
+    # for dataset_name in ti.keys():
+    #     print(f"\t{dataset_name}: {np.mean(ti[dataset_name]):.4f} seconds")
+
+    for dataset_name in datasets.keys():
+        if dataset_name not in maes:
+            continue
+        print(f"Dataset: {dataset_name}")
+        #print(f"\tMean Absolute Error (MAE): {np.mean(maes[dataset_name]):.4f}")
+        print(f"\tAccuracy: {np.mean(accs[dataset_name]):.4f}")
+        print(f"\tTraining Time: {np.mean(tt[dataset_name]):.4f} seconds")
+        print(f"\tImputation Time: {np.mean(ti[dataset_name]):.4f} seconds")
