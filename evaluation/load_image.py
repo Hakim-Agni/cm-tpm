bdef8c599870c9f93daab6639f474404dc948cbf
HakimA2001 <54859731+Hakim-Agni@users.noreply.github.com>
2025-04-28 10:19:45 +0200

    Create load_image.py

---
+++ evaluation/load_image.py
@@ -0,0 +1,81 @@
+from sklearn.datasets import load_digits
+import matplotlib.pyplot as plt
+from matplotlib import colors
+import numpy as np
+import pandas as pd
+from cm_tpm import CMImputer
+
+random_state = 0
+
+# Function to introduce missingness in the dataset
+def introduce_missingness(data, missing_rate=0.1, random_state=42):
+    rng = np.random.RandomState(random_state)  # Ensures reproducibility
+    mask = rng.rand(*data.shape) < missing_rate  # Create mask for missing values
+    data_missing = data.mask(mask)  # Apply mask
+    return data_missing, mask
+
+def show_image(image_data, ax, title):
+    # Convert the 1D array into an 8x8 2D array
+    image_array = np.array(image_data).reshape((8, 8))
+
+    # Create a masked array: mask NaNs
+    masked_array = np.ma.masked_invalid(image_array)
+
+    # Create a custom colormap: grayscale for values, red for NaNs
+    cmap = plt.cm.gray_r
+    cmap = cmap.copy()
+    cmap.set_bad(color='red')  # color for NaNs
+
+    # Plot
+    ax.imshow(masked_array, cmap=cmap, vmin=0, vmax=15)
+    ax.axis('off')
+    ax.set_title(title)
+
+data = pd.DataFrame(load_digits(as_frame=True).frame)
+data = data.drop("target", axis=1)
+
+train_data = data[:1500]
+test_data = data[1500:]
+test_data_missing, _ = introduce_missingness(test_data, missing_rate=0.5, random_state=random_state)
+test_data = test_data
+test_data_missing = test_data_missing
+
+hidden_layers = 5
+neurons_per_layer = 512
+activation = "LeakyReLU"
+batch_norm = True
+dropout_rate = 0.3
+
+model = CMImputer(
+    n_components_train=256,
+    n_components_impute=1024,
+    latent_dim=4,
+    random_state=random_state,
+    verbose=1,
+    # hidden_layers=hidden_layers,
+    # neurons_per_layer=neurons_per_layer,
+    # activation=activation,
+    # batch_norm=batch_norm,
+    # dropout_rate=dropout_rate,
+    # max_iter=100,
+    # tol=0.0001,
+    # lr=0.001,
+    # weight_decay=0.01,
+)
+
+model.fit(train_data)
+
+test_samples = test_data_missing.shape[0]
+
+np.random.seed(random_state)
+index = np.random.randint(low=0, high=test_samples)
+
+test_sample = test_data_missing.iloc[index]
+imputed_sample = model.transform([test_sample])
+
+# Create the figure and axes
+fig, axes = plt.subplots(1, 3, figsize=(9, 6))
+show_image(test_data.iloc[index], axes[0], "Full image")
+show_image(test_sample, axes[1], "Image with missing")
+show_image(imputed_sample, axes[2], "Imputed image")
+plt.show()
