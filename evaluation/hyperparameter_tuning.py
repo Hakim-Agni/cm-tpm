f5509bf2aed414cf9b4616b4d7bab8812177bf90
Hakim-Agni <hakim.a@kpnmail.nl>
2025-06-03 13:55:14 +0200

    Added more evaluation options

--- evaluation/hyperparameter_tuning.py
+++ evaluation/hyperparameter_tuning.py
@@ -4,6 +4,7 @@ import pandas as pd
 import time
 from torch import nn
 from sklearn.metrics import mean_absolute_error, mean_squared_error
+from sklearn.model_selection import train_test_split
 from sklearn.datasets import load_diabetes, load_breast_cancer, load_digits, load_iris, load_linnerud, load_wine
 from sklearn.impute import KNNImputer, SimpleImputer
 from ucimlrepo import fetch_ucirepo
@@ -15,13 +16,16 @@ cm_imputer = CMImputer(
             settings="custom",
             # Add other hyperparameters as needed
         )
-nr_of_runs = 5
+
+# General Settings
+nr_of_runs = 10
 random_state = 42
 missing_rate = 0.1
 show_plots = False       # Whether to show plots of log-likelihoods during training or not
+save_datasets = True     # Whether to save datasets with missing values and imputed values or not
+split_data = False       # Whether to split the data into training and test sets or not
 
 # Dataset Settings
-# Complete datasets
 diabetes = True                 # Medium sized; numerical and integer
 breast_cancer = False            # Large sized; numerical and binary
 digits = False                   # Very large sized; integer; image
@@ -59,7 +63,7 @@ def introduce_missingness(data, missing_rate=0.1, random_state=42):
     data_missing = data.mask(mask)  # Apply mask
     return data_missing, mask
 
-def run_evaluation(cm_imputer=CMImputer(), print_results=True):
+def run_evaluation(cm_imputer=CMImputer(), print_results=True, rand_state=None):
     scores = {}
     likelihoods = {}
     for dataset_name, use_dataset in datasets.items():
@@ -106,16 +110,38 @@ def run_evaluation(cm_imputer=CMImputer(), print_results=True):
             os.makedirs("evaluation/data/wine", exist_ok=True)
             path = "evaluation/data/wine/wine_"
 
-        data.to_csv(path + "complete.csv", index=False)
+        # Save the complete dataset to CSV
+        if save_datasets:
+            data.to_csv(path + "complete.csv", index=False)
 
-        # Introduce 10% missing values
+        if split_data:
+            # Split the data into training and test sets
+            data_train, data_test = train_test_split(data, test_size=0.2, shuffle=True, random_state=rand_state)
+
+            if save_datasets:
+                # Save the training and test sets to CSV
+                data_train.to_csv(path + "train.csv", index=False)
+                data_test.to_csv(path + "test.csv", index=False)
+                if print_results:
+                    print(f"Training set saved as '{dataset_name}_train.csv'")
+                    print(f"Test set saved as '{dataset_name}_test.csv'")
+        else:
+            # Use the complete dataset for training and testing
+            data_test = data.copy()
+            
+        # Introduce missing values
         if not missing:
-            data_missing, mask = introduce_missingness(data, missing_rate=missing_rate, random_state=random_state)
+            data_missing, mask = introduce_missingness(data_test, missing_rate=missing_rate, random_state=rand_state)
+
+        # Use the missing dataset for training and testing
+        if not split_data:
+            data_train = data_missing.copy()
 
         # Save to CSV for testing
-        data_missing.to_csv(path + "with_missing.csv", index=False)
-        if print_results:
-            print(f"Dataset with missing values saved as '{dataset_name}_with_missing.csv'")
+        if save_datasets:
+            data_missing.to_csv(path + "with_missing.csv", index=False)
+            if print_results:
+                print(f"Dataset with missing values saved as '{dataset_name}_with_missing.csv'")
 
         # Impute missing values using chosen imputers
         for imputer_name, use_imputer in imputers.items():
@@ -138,7 +164,7 @@ def run_evaluation(cm_imputer=CMImputer(), print_results=True):
                 name = "Simple Imputer"
 
             start_time = time.time()
-            imputer.fit(data_missing)
+            imputer.fit(data_train)
             train_time = time.time()
             data_imputed = imputer.transform(data_missing)
             impute_time = time.time()
@@ -153,7 +179,7 @@ def run_evaluation(cm_imputer=CMImputer(), print_results=True):
                 }
 
             # Save the imputed dataset (only for CM Imputer)
-            if imputer_name == "cm_imputer":
+            if imputer_name == "cm_imputer" and save_datasets:
                 data_imputed.to_csv(path + "imputed_cm.csv", index=False)
                 if print_results:
                     print(f"Imputed dataset saved as '{dataset_name}_imputed_cm.csv'")
@@ -168,7 +194,7 @@ def run_evaluation(cm_imputer=CMImputer(), print_results=True):
                 if not categorical:     # For non-categorical datasets
                     # Select only the originally missing values for comparison
                     imputed_values = data_imputed.values[mask]
-                    true_values = data.values[mask]
+                    true_values = data_test.values[mask]
                     # Compute Error Metrics
                     mae = mean_absolute_error(true_values, imputed_values)
                     rmse = np.sqrt(mean_squared_error(true_values, imputed_values))
@@ -187,17 +213,17 @@ def run_evaluation(cm_imputer=CMImputer(), print_results=True):
                         "Imputation Time": impute_time - train_time,
                     }
                 else:       # For categorical datasets
-                    data = data.to_numpy()
-                    data = data.astype(str)
+                    data_test = data_test.to_numpy()
+                    data_test = data_test.astype(str)
                     try:
-                        if np.any(np.isnan(data)):
-                            mask = np.logical_and(mask, ~np.isnan(data))
+                        if np.any(np.isnan(data_test)):
+                            mask = np.logical_and(mask, ~np.isnan(data_test))
                     except TypeError:
-                        if np.any(data == "nan"):
-                            mask = np.logical_and(mask, data != "nan")
+                        if np.any(data_test == "nan"):
+                            mask = np.logical_and(mask, data_test != "nan")
                     # Select only the originally missing values for comparison
                     imputed_values = data_imputed.values[mask]
-                    true_values = data[mask]
+                    true_values = data_test[mask]
                     # Compute Accuracy
                     accuracy = (imputed_values == true_values).mean()
                     if print_results:
@@ -216,6 +242,8 @@ if __name__ == "__main__":
     rng = np.random.default_rng(random_state) 
 
     maes = {}
+    rmses = {}
+    corrs = {}
     accs = {}
     tt = {}
     ti = {}
@@ -227,7 +255,7 @@ if __name__ == "__main__":
         rand_state = rng.integers(1e9)
         cm_imputer.set_params(random_state=rand_state)
 
-        score, likelihood = run_evaluation(cm_imputer=cm_imputer, print_results=False)
+        score, likelihood = run_evaluation(cm_imputer=cm_imputer, print_results=False, rand_state=rand_state)
 
         # Store the likelihoods for the first loop only
         for dataset_name in likelihood.keys():
@@ -240,16 +268,22 @@ if __name__ == "__main__":
 
             if dataset_name not in maes:
                 maes[dataset_name] = []
+                rmses[dataset_name] = []
+                corrs[dataset_name] = []
                 accs[dataset_name] = []
                 tt[dataset_name] = []
                 ti[dataset_name] = []
 
-            mae = score[dataset_name, imputer_name].get("MAE", 0)
-            accuracy = score[dataset_name, imputer_name].get("Accuracy", 0)
-            time_train = score[dataset_name, imputer_name].get("Training Time", 0)
-            time_impute = score[dataset_name, imputer_name].get("Imputation Time", 0)
+            mae = score[dataset_name, imputer_name].get("MAE", -1)
+            rmse = score[dataset_name, imputer_name].get("RMSE", -1)
+            correlation = score[dataset_name, imputer_name].get("Correlation", -1)
+            accuracy = score[dataset_name, imputer_name].get("Accuracy", -1)
+            time_train = score[dataset_name, imputer_name].get("Training Time", -1)
+            time_impute = score[dataset_name, imputer_name].get("Imputation Time", -1)
 
             maes[dataset_name].append(mae)
+            rmses[dataset_name].append(rmse)
+            corrs[dataset_name].append(correlation)
             accs[dataset_name].append(accuracy)
             tt[dataset_name].append(time_train)
             ti[dataset_name].append(time_impute)
@@ -258,9 +292,42 @@ if __name__ == "__main__":
     for dataset_name in datasets.keys():
         if dataset_name not in maes:
             continue
+        
+        # Compute mean and confidence intervals for MAE
+        mae_mean = np.mean(maes[dataset_name])
+        mse_std = np.std(maes[dataset_name])
+        mse_int_coef = 1.96 * mse_std / np.sqrt(nr_of_runs)
+        mse_int_top = mae_mean + mse_int_coef
+        mse_int_bottom = mae_mean - mse_int_coef
+
+        # Compute mean and confidence intervals for RMSE
+        rmse_mean = np.mean(rmses[dataset_name])
+        rmse_std = np.std(rmses[dataset_name])
+        rmse_int_coef = 1.96 * rmse_std / np.sqrt(nr_of_runs)
+        rmse_int_top = rmse_mean + rmse_int_coef
+        rmse_int_bottom = rmse_mean - rmse_int_coef
+
+        # Compute mean and confidence intervals for Correlation
+        corr_mean = np.mean(corrs[dataset_name])
+        corr_std = np.std(corrs[dataset_name])
+        corr_int_coef = 1.96 * corr_std / np.sqrt(nr_of_runs)
+        corr_int_top = corr_mean + corr_int_coef
+        corr_int_bottom = corr_mean - corr_int_coef
+
+        # Compute mean and confidence intervals for Accuracy
+        acc_mean = np.mean(accs[dataset_name])
+        acc_std = np.std(accs[dataset_name])
+        acc_int_coef = 1.96 * acc_std / np.sqrt(nr_of_runs)
+        acc_int_top = acc_mean + acc_int_coef
+        acc_int_bottom = acc_mean - acc_int_coef
+
         print(f"Dataset: {dataset_name}")
-        print(f"\tMean Absolute Error (MAE): {np.mean(maes[dataset_name]):.4f}")
-        print(f"\tExact MAEs for each run: {maes[dataset_name]}")
+        if mae_mean != -1:
+            print(f"\tMean Absolute Error (MAE): {mae_mean:.4f}. 95% Confidence Interval: [{mse_int_bottom:.4f}, {mse_int_top:.4f}]")
+            print(f"\tRoot Mean Squared Error (RMSE): {rmse_mean:.4f}. 95% Confidence Interval: [{rmse_int_bottom:.4f}, {rmse_int_top:.4f}]")
+            print(f"\tCorrelation: {corr_mean:.4f}. 95% Confidence Interval: [{corr_int_bottom:.4f}, {corr_int_top:.4f}]")
+        if acc_mean != -1:
+            print(f"\tAccuracy: {acc_mean:.4f}. 95% Confidence Interval: [{acc_int_bottom:.4f}, {acc_int_top:.4f}]")
         #print(f"\tAccuracy: {np.mean(accs[dataset_name]):.4f}")
         print(f"\tTraining Time: {np.mean(tt[dataset_name]):.4f} seconds")
         print(f"\tImputation Time: {np.mean(ti[dataset_name]):.4f} seconds")
